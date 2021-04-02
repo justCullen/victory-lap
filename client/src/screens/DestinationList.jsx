3d04ddcf2802a5f0adb3ca3ca3a20d523fdaa3bf
cbscar <cbscar@gmail.com>
2021-04-02 09:42:38 -0500

    added more seed data, begin data display process

--- client/src/screens/DestinationList.jsx
+++ client/src/screens/DestinationList.jsx
@@ -1,9 +1,20 @@
 import React from 'react'
+import DestinationCard from '../components/DestinationCard';
 
 export default function DestinationList() {
+  const { destinations } = props;
   return (
     <div>
       <h3>DestinationList</h3>
+      {destinations.map(destination => (
+        <DestinationCard
+          id={destination.id}
+          name={destination.name}
+          content={destination.content}
+          user={destination.user.username}
+          comments={destination.comments}
+        />
+        ))}
     </div>
   )
 }
