af20c20097a8aac40b5a3396531f5aad76d7b8c7
cbscar <cbscar@gmail.com>
2021-04-04 13:57:30 -0500

    header bar styling started

--- client/src/layouts/Layout.jsx
+++ client/src/layouts/Layout.jsx
@@ -4,20 +4,20 @@ import './Layout.css';
 export default function Layout(props) {
   const { currentUser, handleLogout } = props;
   return (
-    <div>
-      <header>
-        <Link to='/'><h1>Victory Lap</h1></Link>
+    <div className='main-layout'>
+      <header className='layout-header'>
+        <Link to='/'><div className='layout-title'>Victory Lap</div></Link>
         {
           currentUser ?
             <>
-              <p>{currentUser.username}</p>
-              <button onClick={handleLogout}>Logout</button>
+              <p className='username'>{currentUser.username}</p>
+              <button className='layout-button' onClick={handleLogout}>Logout</button>
             </>
             :
             <Link to='/login'>Login/Register</Link>
         }
       </header>
-      <hr />
+      {/* <hr /> */}
       {props.children}
     </div>
   )
