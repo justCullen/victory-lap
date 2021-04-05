4fec2e4174bbc4b76298024bfed6ba8ad4ee8240
cbscar <cbscar@gmail.com>
2021-04-05 14:55:07 -0500

    api config

--- client/src/services/api-config.js
+++ client/src/services/api-config.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const baseUrl = process.env.NODE_ENV === 'production' ? 'DEPLOY_LINK_GOES_HERE' : 'http://localhost:3000';
+const baseUrl = process.env.NODE_ENV === 'production' ? 'https://victory-lap-api.herokuapp.com/' : 'http://localhost:3000';
 
 const api = axios.create({
   baseURL: baseUrl
